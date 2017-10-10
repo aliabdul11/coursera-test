1bf6a6fe2ff379ded50d6f64334f4cb6e60fdf62
aliabdul11 <32685818+aliabdul11@users.noreply.github.com>
2017-10-11 01:38:46 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,166 @@
+<! DOCTYPE HTML>
+<html>
+<head>
+
+<link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
+<title> Homepage </title>
+	<style>
+	body{
+		margin: 0px;
+		border: 0px;
+		}
+	
+	#header{
+		padding-left:10px;
+		background: cyan;
+		color: black;
+		hight: 900px;
+		width: 100%;
+	}
+	
+
+	
+	#main{
+		padding-lfet:10px;
+		
+		width:100%;
+		height 9000px;
+		float: left;
+	}
+	#cover{
+		background-image: url('images/cover.jpg');
+		height: 800px;
+		color: white;
+	}
+	#cov-desc{
+		padding-top: 20px;
+		padding-bottom: 20px;
+		
+	}
+	
+	
+	p{
+		font-size: 24px;
+		font-family: 'Roboto', sans-serif;
+	}
+	#exp-btn{
+		
+		width: 20%;
+		font-size: 25px;
+		background: rgb(26,188,156, 0.2);
+		color: white;
+		box-shadow: 1px 1px 1px white;
+	}
+	#exp-btn:hover{
+		background: rgb(26,188,156)
+	}
+	#cover2{
+		background: cyan;
+		padding-left: 2%;
+		
+	}
+	#cover3{
+		background: #eee;
+		float: 100%;
+	}
+		
+	#side{
+		
+		background: #eee;
+			width: 100%;
+			float: right;
+	}
+	#contact1{
+		float: right:
+		
+	}
+	#testing{
+		float: 100%;
+	}
+	
+	
+	</style>
+	
+
+
+
+</head>
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	
+<body>
+	<div id="header">
+
+		<h1> Smart Track </h1>
+		
+			<div id="side">
+				<a href="#contacted" style="font-size: 25;"> 
+						Contact Us
+				</a> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+		
+				<a href="#services" style="font-size: 25;"> 
+						Service
+				</a> 
+				&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+		
+				<a href="#devices" style="font-size: 25;"> 
+						Bus No
+				</a> 
+				&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+		
+				<a href="#testing" style="font-size: 25;"> 
+						Prototype
+				</a> 
+			</div>
+	</div>
+
+		<div id="main">
+	
+				
+			<div id="cover">
+		
+			<div id="cov-desc">
+					<center><p> Welcome to the Smart Track services </p>
+					We are having a solution for the fleet managemnet 
+					</center>
+						<h3>* We provide the services for the government buses.</h3>
+						<h3>* We provide the services for the school buses.</h3>
+			</div>
+			
+		</div>
+		<div id="contacted">
+		
+			<a href="https://www.facebook.com/abdulali541">facebook</a>
+		</div>
+		
+		<div id="services">
+			We are providing the services of the public school and the governmenrt buses
+		</div>
+		
+		<div id="devices">
+			<div id="cover2">
+				<h3>#&nbsp; Here need to be php input commond.</3>
+				<h3>#&nbsp; We have login account to the each person for the school</h3>
+				<h3>#&nbsp; After entering the login and password, we will show the buses which follows his specific school and his routes.</h3>
+			</div>
+		</div>
+		
+		<dir id="testing">
+			<div id="cover3">
+				<h4>Please select your school</h4>
+				<form>
+					<input list="school name">
+					<datalist id="school name">
+						<option value="Ideal public school">
+						<option value="Sangram Singh Baghel Inter Coleege">
+						<option value="Indian Institute of Technology">
+						<option value="some abcd schools">
+					</datalist>
+				</form>
+				<a href="http://deep-mile.westindia.cloudapp.azure.com/machineCoords5.html">Go to the current location</a>
+			</div>
+		</div>
+			
+		
+</body>
+
+</html>
